9d5252a0d033adcb444fac1b70f1f7c4e2a94e78
young209 <sunsky3002@naver.com>
2021-11-17 11:03:45 +0900

    init

---
+++ src/main/java/com/youngg209/TestProjectYoungG209Application.java
@@ -0,0 +1,13 @@
+package com.youngg209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectYoungG209Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectYoungG209Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngg209/TestProjectYoungG209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngg209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectYoungG209ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
